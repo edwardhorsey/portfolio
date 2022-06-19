a45fb4948f6fe691973b84ba9edf37d17e1a80ea
Edward Horsey <edward.horsey@warnermusic.com>
2022-06-19 10:27:34 +0100

    update OG img

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,7 @@
 export const title = 'Edward Horsey Portfolio';
 export const description = 'Edward Horsey is a full stack software developer based in London, UK.';
 export const siteUrl = 'https://www.edwardhorsey.dev';
-export const ogImage = '';
+export const ogImage = 'https://www.edwardhorsey.dev/img/og.png';
 export const favicon = `/img/favicon.png`;
 
 export const IS_DEV = process.env.NODE_ENV === 'development';

b43ef634ef8e92c08a30187ec77407b464cddca1
Edward Horsey <edwardhorsey@protonmail.com>
2024-01-02 14:08:13 +0000

    add photography redirect

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 module.exports = {
   reactStrictMode: true,
   assetPrefix: ".",
+  async redirects() {
+    return [
+      {
+        source: '/photography',
+        destination: 'https://www.flickr.com/photos/198757426@N02',
+        permanent: true,
+      },
+    ]
+  },
 };

ab9dfb36812543da24a9a83fb50f5706d3e8390f
Edward Horsey <edwardhorsey@protonmail.com>
2024-01-02 13:28:42 +0000

    Add flickr

--- src/components/ContactIcons.tsx
+++ src/components/ContactIcons.tsx
@@ -1,7 +1,7 @@
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
-import { faAt } from '@fortawesome/free-solid-svg-icons';
+import { faAt, faCamera } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useInView } from 'react-intersection-observer';
 import { animated, useTrail } from 'react-spring';
 
@@ -9,12 +9,13 @@ const items = [
     { key: 'a', url: 'mailto:edwardhorsey@proton.me', icon: faAt },
     { key: 'b', url: 'https://www.linkedin.com/in/edward-horsey-5a69151a1', icon: faLinkedin },
     { key: 'c', url: 'https://www.github.com/edwardhorsey', icon: faGithub },
+    { key: 'd', url: 'https://www.flickr.com/photos/198757426@N02', icon: faCamera },
 ];
 
 const ContactIcons = (): JSX.Element => {
     const [ref, inView] = useInView();
     const [enter, setEnter] = useState(false);
-    const trail = useTrail(3, {
+    const trail = useTrail(4, {
         distance: enter ? 0 : -4,
         opacity: enter ? 1 : 0,
         config: { mass: 18, tension: 2500, friction: 200 },
@@ -28,7 +29,7 @@ const ContactIcons = (): JSX.Element => {
     return (
         <ul
             ref={ref}
-            className="flex gap-6 lg:gap-3 lg:pt-2 justify-center lg:justify-start lg:flex-col w-full text-4xl"
+            className="flex gap-6 lg:gap-3 lg:pt-2 lg:pl-2 justify-center lg:justify-start lg:flex-col w-full text-4xl"
         >
             {trail.map((styles, idx) => (
                 <animated.li
